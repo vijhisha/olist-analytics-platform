5ff7382864142e39e54717fb96221e6a01584e3a
Vijhisha <bhandary.vijhisha@gmail.com>
2026-07-17 10:37:55 +0200

    Adding mart_gmv_daily with category share using window function

---
+++ olist_analytics/models/marts/mart_gmv_daily.sql
@@ -0,0 +1,29 @@
+with category_gmv_aov_cte as (
+    select 
+        date(fct_orders.order_purchase_timestamp) as order_date,
+        fct_order_items.product_category_name,
+        fct_order_items.product_category_name_english,
+        sum(fct_order_items.item_total) as daily_gmv,
+        count(distinct fct_order_items.order_id) as order_count,
+        sum(fct_order_items.item_total) / count(distinct fct_order_items.order_id) as daily_aov
+
+    from {{ref ('fct_order_items')}} as fct_order_items
+        left join {{ref ('fct_orders')}} fct_orders on fct_order_items.order_id = fct_orders.order_id
+    where fct_orders.order_status not in ('canceled', 'unavailable')
+    group by 
+        date(fct_orders.order_purchase_timestamp),
+        fct_order_items.product_category_name,
+        fct_order_items.product_category_name_english
+
+)
+
+select 
+    order_date,
+    product_category_name,
+    product_category_name_english,
+    daily_gmv,
+    order_count,
+    daily_aov,
+    daily_gmv / sum(daily_gmv) over (partition by order_date) as category_gmv_share 
+
+from category_gmv_aov_cte
